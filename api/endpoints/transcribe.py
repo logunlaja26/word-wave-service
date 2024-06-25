03dcefddba5c149af12c793ea4473f38ffbbec76
logunlaja26 <logunlaja26@gmail.com>
2024-06-24 19:52:26 -0500

    cleaned up transcription endpoint

--- api/endpoints/transcribe.py
+++ api/endpoints/transcribe.py
@@ -1,26 +1,21 @@
-from api.datastore.firestore import audio_url
 from fastapi import APIRouter, Depends, HTTPException, Query
 from transcribeFunction import transcribe_audio
+from transcribeProcessor import transcribe_audio_replicate
+from pydantic import BaseModel
 from fileLocationFinder import file_location
 
 
 router = APIRouter()
 
+class TranscribeRequest(BaseModel):
+    url: str
 
-# @router.post("/transcribe")
-# async def transcibeAudio(file_path: str = Depends(file_location)):
-#     try:
-#         transcript = transcribe_audio(file_path)
-#         return {"transcript": transcript.text}
-#     except Exception as e:
-#         print("Error...", e)
-#         raise HTTPException(status_code=500, detail="An error occurred during transcription.")
-    
+
 @router.post("/transcribe")
-async def transcibeAudio(file_path: str = Depends(audio_url)):
+async def transcibeAudio(request: TranscribeRequest):
     try:
-        transcript = transcribe_audio(file_path)
-        return {"transcript": transcript.text}
+        transcript = transcribe_audio_replicate(request.url)
+        return transcript
     except Exception as e:
         print("Error...", e)
-        raise HTTPException(status_code=500, detail="An error occurred during transcription.")    
+        raise HTTPException(status_code=500, detail="An error occurred during transcription.")  
